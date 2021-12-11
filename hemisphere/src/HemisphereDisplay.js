dc21a3138dec8495cb1215ee104a7a5e015b8034
snegib <aaravbisht20022016@gmail.com>
2021-12-11 22:01:21 +0530

    add config object and style up

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import northenPic from './image/northen-hemisphere.gif';
+import northernPic from './image/northern-hemisphere.gif';
 import southernPic from './image/southern-hemisphere.jpg';
+
+const hemisphereConfig = {
+  Northern: {
+    text: 'It is northern hemisphere.',
+    picture: northernPic,
+  },
+  Southern: {
+    text: 'It is southern hemisphere.',
+    picture: southernPic,
+  },
+};
 
 const HemisphereDisplay = ({ latitude }) => {
   // deconstruct syntax 2016
   // Ternary expression
-  const hemisphere =
-    latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere';
-  const picture = latitude > 0 ? northenPic : southernPic;
+  const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+  const { text, picture } = hemisphereConfig[hemisphere];
   return (
-    <div>
-      <img src={picture} alt="" />
-      {hemisphere}
+    <div className={hemisphere}>
+      <div className="ui raised text container segment">
+        <div className="image">
+          <img src={picture} alt="" />
+        </div>
+        <div className="ui teal bottom attached label">
+          <h1>{text}</h1>
+        </div>
+      </div>
     </div>
   );
 };
