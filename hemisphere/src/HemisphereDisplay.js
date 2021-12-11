86ce99a48b261368211c0586829ccf2d6d3810b7
snegib <aaravbisht20022016@gmail.com>
2021-12-11 20:55:39 +0530

    ternary operator and hemisphere picture show

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import northenPic from './image/northen-hemisphere.gif';
+import southernPic from './image/southern-hemisphere.jpg';
 
-const HemisphereDisplay = (props) => {
-    console.log(props);
-  return <div></div>;
+const HemisphereDisplay = ({ latitude }) => {
+  // deconstruct syntax 2016
+  // Ternary expression
+  const hemisphere =
+    latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere';
+  const picture = latitude > 0 ? northenPic : southernPic;
+  return (
+    <div>
+      <img src={picture} alt="" />
+      {hemisphere}
+    </div>
+  );
 };
 
 export default HemisphereDisplay;
