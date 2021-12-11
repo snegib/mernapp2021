3579b068f7c9e15edce3955d6b44fc4b503d0d49
snegib <aaravbisht20022016@gmail.com>
2021-12-11 20:04:48 +0530

    passing state as props

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const HemisphereDisplay = () => {
+const HemisphereDisplay = (props) => {
+    console.log(props);
   return <div></div>;
 };
 
--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HemisphereDisplay from './HemisphereDisplay';
 
 /* const App = () => {
   window.navigator.geolocation.getCurrentPosition(
@@ -15,25 +16,29 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   // any time when this class is instance is created this construction function is automatically called before anything else.
-  constructor(props) {
-    /* // automatically called props */
-    super(props);
-    this.state = { latitude: null, errorMessage: '' };
+  //   constructor(props) {
+  //     /* // automatically called props */
+  //     super(props);
+  //     this.state = { latitude: null, errorMessage: '' };
+
+  //   }
+
+  // this line is equivalent to above constructor function inside. this is the shorter way to do. just remove construction function and initialize state without calling 'this'
+  state = { latitude: null, errorMessage: '' };
+  componentDidMount() {
+    console.log('componentDidMount');
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ latitude: position.coords.latitude });
       },
       error => {
-        this.setState({errorMessage: error.message})
+        this.setState({ errorMessage: error.message });
       }
     );
   }
-  componentDidMount(){
-      console.log("componentDidMount");
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
   }
-  componentDidUpdate(){
-    console.log("componentDidUpdate");
-}
   render() {
     /* we should not call this type of functionality in render method, the reason is that render method is called all the time. and this functionality will change the position as per user current position every time */
     /* window.navigator.geolocation.getCurrentPosition(
@@ -44,14 +49,13 @@ class App extends React.Component {
         console.log(error);
       }
     ); */
-    if(this.state.errorMessage && !this.state.latitude){
-        return <div>{this.state.errorMessage}</div>
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>{this.state.errorMessage}</div>;
     }
-    if(!this.state.errorMessage && this.state.latitude){
-        return <div>{this.state.latitude}</div>
-    }
-    else {
-        return <div>Loading...</div>
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div><HemisphereDisplay latitude={this.state.latitude}/></div>;
+    } else {
+      return <div>Loading...</div>;
     }
   }
 }
