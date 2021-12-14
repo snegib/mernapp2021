71d31846bdf531aa759fc5fe2bb9f9ba066564e6
snegib <aaravbisht20022016@gmail.com>
2021-12-14 20:56:10 +0530

    image-list invoke callback in childerens

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import SearchInput from './SearchInput';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '30px' }}>
-      <SearchInput />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+  onSearchSubmit(entry) {
+    console.log(entry);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '30px' }}>
+        <SearchInput onSearchSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -17,7 +17,7 @@ class SearchInput extends React.Component {
   /* onFormSubmit(event) { */
   onFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state.entry);
+    this.props.onSearchSubmit(this.state.entry)
   };
   render() {
     return (
