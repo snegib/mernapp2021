2ed74aa99d1e467f5b983090d55cf2b15b02b6b7
snegib <aaravbisht20022016@gmail.com>
2021-12-14 21:28:47 +0530

    image-list setting the state in react after fetching data

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {images: []}
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
   async onSearchSubmit(entry) {
@@ -12,11 +13,13 @@ class App extends React.Component {
     const response = await axios.get(`
     https://pixabay.com/api/?key=24816258-76261dbb937f3f3e7cc22bcde&q=${entry}&image_type=photo`);
     console.log(response.data.hits);
+    this.setState({images: response.data.hits})
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '30px' }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        we have {this.state.images.length} images.
       </div>
     );
   }
