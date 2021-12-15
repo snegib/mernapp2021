f20d407ad3611db947a0824260688bcfa917b2f8
snegib <aaravbisht20022016@gmail.com>
2021-12-15 20:02:01 +0530

    react-router setting up Redux

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* STEP 1
+// how does react interact with this store or how do we associate this store with a React application. for this we need to call Provider component and wrap our application with it and pass store to it. 
+*/
+const store =
+  createStore(
+    rootReducer
+  ); /* STEP 5 - pass reducer, now reducer associate with store */
+
+/* STEP 2
+we need to create a reducer and put into this store. for this i have create a 'reducers' folder in src  */
+
+ReactDOM.render(
+  <Provider store={store}> {/*  STEP 5 - now store pass to our APP */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+); 
---
+++ react-router/src/reducers/rootReducer.js
@@ -0,0 +1,16 @@
+/* STEP 3
+set the initial state
+*/
+const initialState = {
+  cards: [],
+};
+
+/* STEP 2 
+this reducer takes two arguments, first initial state and second is action 
+*/
+const rootReducer = (state = initialState, action) => {
+  /* STEP 4 - pass initialState */
+  return state;
+};
+
+export default rootReducer;
