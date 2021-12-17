70684f5d0c6d34709ef42414705740dc978324b8
snegib <aaravbisht20022016@gmail.com>
2021-12-17 23:54:35 +0530

    image-list minor fix

--- image-list/src/components/ImageList.js
+++ image-list/src/components/ImageList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ImageList = props => {
-  const image = props.images.map((image) => {
+  const image = props.images.map(image => {
     return <img src={image.webformatURL} alt="" key={image.id} />;
   });
   return <div>{image}</div>;
