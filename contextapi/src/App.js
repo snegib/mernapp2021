2bb865dab8c8b6b542334c851cc02ae920546c3e
snegib <aaravbisht20022016@gmail.com>
2021-12-21 20:07:01 +0530

    contextapi createContext for our project

--- contextapi/src/App.js
+++ contextapi/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
 import Navbar from './components/NavBar';
 import TodoList from './components/TodoList';
 const App = () => {
@@ -11,4 +10,8 @@ const App = () => {
   );
 };
 
-render.ReactDOM('<App />', document.querySelector('#root'));
+export default App;
+
+/* STEP 1 [NOTE TOTAL STEP IS **]
+we have 'Navbar' and 'TodoList' in place and now we want change color theme. so in this project we are going to create a theme context. for this, we already created a 'contexts' folder inside 'src'. So now I can create a new file, inside 'contexts' folder and name it 'ThemeContext.js' as our context is going to be related to theme.
+ */
--- contextapi/src/contexts/ThemeContext.js
+++ contextapi/src/contexts/ThemeContext.js
@@ -1,1 +1,8 @@
+/*  STEP 2
+This is where we're going to create our theme in context. import 'createContext', this function will create context for us */
+import React, { createContext } from 'react';
 
+/* STEP 3
+export  'createContext'
+So this is going to create a context for us, and it's going to be stored in this constant(const) theme context.*/
+export const ThemeContext = createContext();
