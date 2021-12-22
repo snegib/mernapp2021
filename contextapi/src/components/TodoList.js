677f2a170d07829c6e9c49b03917d7a391baa33e
snegib <aaravbisht20022016@gmail.com>
2021-12-22 19:47:32 +0530

    contextapi consume data in TodoList component

--- contextapi/src/components/TodoList.js
+++ contextapi/src/components/TodoList.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class TodoList extends React.Component {
+  static contextType = ThemeContext;
   render() {
+    console.log(this.context);
+    const { isDarkTheme, darkTheme, lightTheme } = this.context;
+    const theme = isDarkTheme ? darkTheme : lightTheme;
     return (
-      <div className="ui list">
+      <div
+        style={{
+          background: theme.background,
+          color: theme.text,
+          height: '140px',
+          textAlign: 'center',
+        }}
+      >
         <p className="item">Overview</p>
         <p className="item">Contact</p>
         <p className="item">Support</p>
