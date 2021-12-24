f43f8b0d50ec5834c048fe6fdc2a84491fe2e15b
snegib <aaravbisht20022016@gmail.com>
2021-12-24 21:22:21 +0530

    hooks project setup

---
+++ hooks/src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Pay bills</li>
+        <li>Do your homework</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
