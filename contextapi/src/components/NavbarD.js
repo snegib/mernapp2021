ab0e5b78e7e2e50bb74177976080e017b90f718b
snegib <aaravbisht20022016@gmail.com>
2021-12-26 22:24:04 +0530

    contextapi create multiple context with useContext hook

---
+++ contextapi/src/components/NavbarD.js
@@ -0,0 +1,78 @@
+/* STEP 9 [useContext] 
+so one of the cool things about this use useContext, hook, is that we can use it as many times as we want inside a component. For example, we can consume as many different contexts as we want inside this 'Navbar' component using the useContext hook. so now we are using two different context inside this component. one is 'ThemeContext' and second is 'AuthContext'. 
+now turn this as functional component. */
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
+
+const NavbarD = () => {
+  const { isDarkTheme, darkTheme, lightTheme } = useContext(ThemeContext);
+  const { isLoggedIn, changeAuthStatus } = useContext(AuthContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+
+  return (
+    <nav
+      style={{
+        background: theme.background,
+        color: theme.text,
+        height: '120px',
+      }}
+    >
+      <h2 style={{ textAlign: 'center' }}>Oak Academy</h2>
+      <p onClick={changeAuthStatus} style={{ textAlign: 'center' }}>
+        {isLoggedIn ? 'Logged in' : 'logged out'}
+      </p>
+      <div className="ui three buttons">
+        <button className="ui button">Overview</button>
+        <button className="ui button">Contact</button>
+        <button className="ui button">Support</button>
+      </div>
+    </nav>
+  );
+};
+
+/* class Navbar extends React.Component {
+  render() {
+    return (
+      <AuthContext.Consumer>
+        {authContext => {
+          return (
+            <ThemeContext.Consumer>
+              {themeContext => {
+                const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+                const { isLoggedIn, changeAuthStatus } = authContext;
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+
+                console.log('context in navbar render ', themeContext);
+                return (
+                  <nav
+                    style={{
+                      background: theme.background,
+                      color: theme.text,
+                      height: '120px',
+                    }}
+                  >
+                    <h2 style={{ textAlign: 'center' }}>Oak Academy</h2>
+                    <p
+                      onClick={changeAuthStatus}
+                      style={{ textAlign: 'center' }}
+                    >
+                      {isLoggedIn ? 'Logged in' : 'logged out'}
+                    </p>
+                    <div className="ui three buttons">
+                      <button className="ui button">Overview</button>
+                      <button className="ui button">Contact</button>
+                      <button className="ui button">Support</button>
+                    </div>
+                  </nav>
+                );
+              }}
+            </ThemeContext.Consumer>
+          );
+        }}
+      </AuthContext.Consumer>
+    );
+  }
+} */
+
+export default NavbarD;
