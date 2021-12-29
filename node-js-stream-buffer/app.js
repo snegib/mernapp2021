79ac1037ced08054858f0b12d8dfdfe3c2045c09
snegib <aaravbisht20022016@gmail.com>
2021-12-29 23:26:17 +0530

    node-js-stream-buffer read and write data with PIPE

--- node-js-stream-buffer/app.js
+++ node-js-stream-buffer/app.js
@@ -9,3 +9,22 @@ readStream.on('data', chunk => {
   console.log('new data received ', chunk);
   writeStream.write(chunk);
 });
+
+/* STEP 1 [PIPE Method]
+elegant way to read and write data, which is pipe  after that we want to sent it client. for that we need to create server*/
+const readStream1 = fs.createReadStream(__dirname + '/read-me1.txt');
+const writeStream1 = fs.createWriteStream(__dirname + '/write-me1.txt');
+readStream1.pipe(writeStream1);
+
+/*  STEP 2 [PIPE Method] */
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+  });
+  const readStream2 = fs.createReadStream(__dirname + '/read-me2.txt');
+  readStream2.pipe(
+    res
+  ); /* read-me text in the browser.. run node app in terminal and see the result */
+});
+console.log('port 3000');
+server.listen(3000, '127.0.0.1');
