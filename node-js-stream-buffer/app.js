e184b787cf7a4fc77f30effc2ebd8621acd80dd2
snegib <aaravbisht20022016@gmail.com>
2021-12-29 23:08:43 +0530

    node-js-stream-buffer write data with stream

--- node-js-stream-buffer/app.js
+++ node-js-stream-buffer/app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 /* we are reading file but this time will do with 'stream' method. previous method you can check in 'nod-js-tutorial'. stream method is very clear */
 const readStream = fs.createReadStream(__dirname + '/read-me.txt');
+/* write stream */
+const writeStream = fs.createWriteStream(__dirname + '/write-me.txt');
 readStream.on('data', chunk => {
   console.log('new data received ', chunk);
+  writeStream.write(chunk);
 });
