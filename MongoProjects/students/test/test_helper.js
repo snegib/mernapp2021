048da6c315d76a21ff9e6588d28d2d593dc147f9
snegib <aaravbisht20022016@gmail.com>
2022-01-02 22:50:30 +0530

    MongoProjects drop collection and use isNew method

--- MongoProjects/students/test/test_helper.js
+++ MongoProjects/students/test/test_helper.js
@@ -11,3 +11,16 @@ mongoose.connection
   .on('error', error => {
     console.log('error occured ', error);
   });
+
+/*  beforeEach is a hook and it will run before each test, so it'll empty out our database and then run our test. Now, the challenging part here is that we've got to find our collection of students and drop all the records inside.  when Mongoose starts to reach out to Mangodb, it takes a little time to show the
+results. Whether it's a failure or a success. So just like that, this idea can apply to every operation on a collection. So, you know, the Mongoose is asynchronous. So because of that, we've got to be sure that we pause the testing environment and tell Mocha to stop until this rather long operation is over.
+So here's where we use callback.
+(done) => {
+  Mongoose.connection.collections.students.drop();
+}
+
+This is the done call back. After the long running process is done. We'll call this done callback, which is provided by Mocha automatically. Now, the tests will continue after we call this done.*/
+beforeEach(done => {
+  mongoose.connection.collections.students.drop();
+  done();
+});
