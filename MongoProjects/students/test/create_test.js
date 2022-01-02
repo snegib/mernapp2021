35e79053a3f94308eec79ea5b7a6f25eae125a2d
snegib <aaravbisht20022016@gmail.com>
2022-01-02 22:09:26 +0530

    MongoProjects create and save model

--- MongoProjects/students/test/create_test.js
+++ MongoProjects/students/test/create_test.js
@@ -1,11 +1,16 @@
 /* in Mocha we have 'describe' and 'it' function  */
 
 const assert = require('assert');
+const Student = require('../src/student');
 
 /* describe function have two arguments. first is 'String' and second is 'callback function' */
 describe('create the first data', () => {
   it('save the student', () => {
     /* we make assertion here. assertion is compare two values. it will be a boolean  and if everything go well with the code expectation then out test passes. to see the result run 'npm run test' */
     assert(2 + 3 == 5);
+    const suman = new Student({
+      name: 'Suman',
+    });
+    suman.save(); /* to save in database. persist to database . mongoose will insert into this to database*/
   });
 });
