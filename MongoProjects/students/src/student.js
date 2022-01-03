fb98e007cec15fc715d74c8c27b307c89384cc20
snegib <aaravbisht20022016@gmail.com>
2022-01-03 22:40:21 +0530

    MongoProjects validation 2

--- MongoProjects/students/src/student.js
+++ MongoProjects/students/src/student.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ArticleSchema = require('../src/article_schema');
 
 /* schema says what property we expect in a model */
 const StudentSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Name is required']
+    required: [true, 'Name is required'],
+    validate: {
+      validator: name => name.length > 2,
+      message: 'Name is too short',
+    },
   },
   studentNumber: Number,
   articleCount: Number,
   grade: Number,
+  article: [ArticleSchema],
 });
 
 const Student = mongoose.model('student', StudentSchema);
