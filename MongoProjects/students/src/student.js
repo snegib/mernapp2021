50ebabbf91efb0c8ca5421ea6a9cfca933f6175b
snegib <aaravbisht20022016@gmail.com>
2022-01-03 20:39:13 +0530

    MongoProjects use  operator

--- MongoProjects/students/src/student.js
+++ MongoProjects/students/src/student.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 const StudentSchema = new Schema({
   name: String,
   studentNumber: Number,
+  articleCount: Number,
+  grade: Number,
 });
 
 const Student = mongoose.model('student', StudentSchema);
---
+++ MongoProjects/students/test/updatebyoperators_test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const Student = require('../src/student');
+
+describe('delete the records ', () => {
+  let suman;
+  beforeEach(done => {
+    suman = new Student({
+      name: 'Suman',
+      studentNumber: 2500,
+      articleCount: 5,
+      grade: 10,
+    });
+
+    suman.save().then(() => done());
+  });
+
+  it('update grades with operators', async () => {
+    const artCount = await Student.findOne({ name: 'Suman' });
+    const student = await Student.updateOne(
+      { name: 'Suman' },
+      { $mul: { grade: artCount.articleCount } }
+    );
+    const res = await Student.find({ name: 'Suman' });
+    assert(res[0].grade === 50);
+    console.log(res);
+  });
+});
