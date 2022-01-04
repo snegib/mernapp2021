93aa0769b0d56f38937e7c29dc98229eb7c4c988
snegib <aaravbisht20022016@gmail.com>
2022-01-04 22:46:34 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN general setup bodyparser and cors

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/index.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/index.js
@@ -2,3 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app =
+  express(); /* initialize app with express(), so that we can use different methods with it */
+app.use(bodyParser.json({ limit: '32mb', extended: true }));
+/* Now in here, we have to do a limit is equal to 32 megabytes. I just did that because we're going to be sending some images which can be large in size and and we're going to say extended is equal to true just like that. */
+
+app.use(
+  bodyParser.urlencoded({ limit: '32mb', extended: true })
+); /*here, just setting up the body passer so that we can properly send our requests. */
+
+
+app.use(cors())
