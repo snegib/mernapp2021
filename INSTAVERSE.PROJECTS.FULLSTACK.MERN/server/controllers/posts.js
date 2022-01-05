ec8607308a29af4bbe2d85960b2abad5d7d78cf7
snegib <aaravbisht20022016@gmail.com>
2022-01-05 19:18:18 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN adding more controller or post request

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/controllers/posts.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/controllers/posts.js
@@ -1,5 +1,42 @@
-/* we kept login of 'server=> routes=> posts.js' logic inside this file. because this file could have length so we keep logic here and make 'server=> routes=> posts.js' short just to call these logics there. */
+/* we kept logic of 'server=> routes=> posts.js' inside this 'server=> controller=> posts.js' . because this file could have length because of logics. so we keep logic inside 'server=> controller=> posts.js' and make 'server=> routes=> posts.js' short just to call these logics there. */
 
-export const getPosts = (req, res) => {
+/* we are going to import PostMessage  so that we can access real model schema access*/
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+  /* each call back function is going to have try and catch block   
+    try{
+
+    }catch(error){
+        
+    }*/
+  try {
+    /* try happen if code is successful. Post message that find or finding something inside of a model takes time. which means that it is an asynchronous action. for that reason we have to add 'await' in front of it
+      const postMessages = await PostMessage.find()
+      
+       and therefore  we have to make this function asynchronous
+       
+       export const createPosts =async (req, res) => { */
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+    /* if everything fine we will say res.status(200, which means everything went okey. and then we are going to return JSON which is going to simply be an array of all message that we have) */
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
   res.send('Post works!');
+};
+
+export const createPost = async (req, res) => {
+  /* with POST request you have access to something know as a request.body. after this we are going to move to front end to create a form and basic layout for making different POSTS.  */
+  const body = req.body;
+
+  /* then we need to create a new post. and then pass the values that we are receiving from  the body  */
+  const newPost = new PostMessage(body);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost); /* https://httpstatus.com/ */
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
