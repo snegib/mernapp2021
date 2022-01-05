99e16ced64e3b222d306e96c91aa2de000d6766a
snegib <aaravbisht20022016@gmail.com>
2022-01-05 19:58:07 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN implement material ui

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/App.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import instaverse from './images/instaverse.jpg';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography varient="h2" align="center">
+          Instaverse
+        </Typography>
+        <img src={instaverse} alt="instaverse" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
