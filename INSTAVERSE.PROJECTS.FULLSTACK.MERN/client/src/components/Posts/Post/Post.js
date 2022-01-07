52433f5251ae9c94df4855b1d1ed953e292e243a
snegib <aaravbisht20022016@gmail.com>
2022-01-07 20:54:23 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN display post component

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Posts/Post/Post.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Posts/Post/Post.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import useStyles from './styles';
-
-const Post = () => {
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@material-ui/core';
+/* install 'npm install @material-ui/icons */
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <div className={classes.grid}>Post</div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      >
+        {' '}
+      </CardMedia>
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+
+      <div className={classes.overlay2}>
+        <Button style={{ color: 'white' }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="medium" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map(tag => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        {post.title}
+      </Typography>
+
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button color="primary" size="small" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small">
+            &nbsp; Like &nbsp;
+            {post.likeCount}
+          </ThumbUpAltIcon>
+        </Button>
+        <Button color="primary" size="small" onClick={() => {}}>
+          <DeleteIcon fontSize="small">Delete {post.likeCount}</DeleteIcon>
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
