2422ca1ec27960556604d0d218f408f22556f504
snegib <aaravbisht20022016@gmail.com>
2022-01-07 12:29:47 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN handle onChange callback

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Form/Form.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Form/Form.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 const Form = () => {
   const classes = useStyles();
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
   const handleSubmit = () => {};
   return (
     <Paper>
@@ -13,7 +20,17 @@ const Form = () => {
         onSubmit={handleSubmit}
       ></form>
       <Typography variant="h6">Creating a Post</Typography>
-      <TextField name="creator" variant="outlined" label="Creator" fullWidth value={} onChange={}></TextField>
+      <TextField
+        name="creator"
+        variant="outlined"
+        label="Creator"
+        fullWidth
+        value={postData.creator}
+        onChange={e => {
+          setPostData({ ...postData, creator: e.target, value });
+        }}
+      ></TextField>
+      {/* What we actually have to do is first spread the postData. So that's ...postData And then we have a creator right there. That means that in every text field, if we do the same thing, but only change the last property. Well, that means all the data is going to persist while changing only this specific property of that specific text field. */}
     </Paper>
   );
 };
