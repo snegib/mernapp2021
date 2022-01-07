10920e2bed4ae44f7c26ff45ceb28057c5b427e0
snegib <aaravbisht20022016@gmail.com>
2022-01-07 12:21:54 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN building Form component

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Form/Form.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Form/Form.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import useStyles from './styles';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 const Form = () => {
   const classes = useStyles();
-  return <div className={classes.fileInput}>Form</div>;
+  const handleSubmit = () => {};
+  return (
+    <Paper>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      ></form>
+      <Typography variant="h6">Creating a Post</Typography>
+      <TextField name="creator" variant="outlined" label="Creator" fullWidth value={} onChange={}></TextField>
+    </Paper>
+  );
 };
 
 export default Form;
