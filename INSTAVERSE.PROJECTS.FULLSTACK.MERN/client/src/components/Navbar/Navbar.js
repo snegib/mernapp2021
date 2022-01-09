582caab8d1f7dbf54b4a8ac68f2af5c48b67ffde
snegib <aaravbisht20022016@gmail.com>
2022-01-10 00:10:08 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN add some more presentational code to Navbar component

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Navbar/Navbar.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/client/src/components/Navbar/Navbar.js
@@ -1,21 +1,64 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Typography, Toolbar, Button } from '@material-ui/core';
 import useStyles from './styles';
 import instaverse from '../../images/instaverse.jpg';
 
 export const Navbar = () => {
   const classes = useStyles();
+  const user = null;
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Typography className={classes.heading} varient="h2" align="center">
-        Instaverse
-      </Typography>
-      <img
-        className={classes.image}
-        src={instaverse}
-        alt="instaverse"
-        height="60"
-      />
+      <div className={classes.brandContainer}>
+        <Typography
+          className={classes.heading}
+          component={Link}
+          to="/"
+          variant="h2"
+          align="center"
+        >
+          Instaverse
+        </Typography>
+        <img
+          className={classes.image}
+          src={instaverse}
+          alt="instaverse"
+          height="60"
+        />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+            //   onClick={logout}
+              color="secondary"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
