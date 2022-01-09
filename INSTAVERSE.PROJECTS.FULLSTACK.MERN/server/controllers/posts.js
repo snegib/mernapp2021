b47578b35353e689b84ab4d5dad48315d8aa598c
snegib <aaravbisht20022016@gmail.com>
2022-01-09 19:55:52 +0530

    INSTAVERSE.PROJECTS.FULLSTACK.MERN update functionality from backend

--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/controllers/posts.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/controllers/posts.js
@@ -2,6 +2,7 @@
 
 /* we are going to import PostMessage  so that we can access real model schema access*/
 import PostMessage from '../models/postMessage.js';
+import { Mongoose } from 'mongoose';
 
 export const getPosts = async (req, res) => {
   /* each call back function is going to have try and catch block   
@@ -39,4 +40,23 @@ export const createPost = async (req, res) => {
   } catch (error) {
     return res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } =
+    req.params; /* extract the id which we want update by destructuring. and also if we want, we can rename it { id: _id }.
+   /posts/123 */
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    /* Here we're going to do one simple check to see if _id is really a mongoose object ID. So to do that first, we have to import Mongoose from Mongoose and then check if it's not valid (!mongoose.Types.ObjectId.isValid(_id))*/
+    return res.status(404).send('No post with that ID');
+  }
+  /* else if the ID is there then we do update */
+  /* We're going to call our model, which is the post message, and then we're going to call a method called 'findByIdAndUpdate'. So there we need to pass the _id as the first parameter. here's a second parameter or the argument we have to pass the whole updated 'post', where are we receiving the data for the updates? Well, we are receiving it from the req.body. That is going to be sent from the front end. Now in here, we can pass that post, and finally, we have to specify new to be equal to true. So that we can actually receive the updated version of that post.*/
+  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  /* so now we have access to the updatePost, and it also is going to be updated in the database. And finally, we can do res.json and we can send over that updated post. So that's going to be it for the update route and the controller. */
+  res.json(updatePost);
 };
--- INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/routes/posts.js
+++ INSTAVERSE.PROJECTS.FULLSTACK.MERN/server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getPosts, createPost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
 const router = express.Router();
 
 /* //localhost:5000 */
 router.get('/', getPosts);
 /* adding more routes */
 router.post('/', createPost);
+/* patch for updating files */
+router.patch('/:id', updatePost);
 export default router;
